92700cf040edf30186b726255eb67087100f6b87
Apurba Halder <apurbahalder.rnc@gmail.com>
2026-01-06 09:50:03 +0530

    Added more files and modified existing files for Day2 and Day3 exercises.

--- Day2/CountMain.java
+++ Day2/CountMain.java
@@ -1,10 +1,17 @@
-class CountMain{
+class ObjectCount {
+    static int count = 0;
+    ObjectCount(){
+        count = count + 1;
+    }
+}
+
+public class CountMain{
 	public static void main(String[] args){
+
 		ObjectCount a = new ObjectCount();
 		ObjectCount b = new ObjectCount();
-		ObjectCount d = new ObjectCount();
 		ObjectCount c = new ObjectCount();
-		ObjectCount o = new ObjectCount();
+
 		System.out.println(ObjectCount.count);
-		}
 	}
+}
--- Day2/ObjectCount.java
+++
@@ -1,7 +0,0 @@
-public class ObjectCount{
-static int count=0;
-ObjectCount(){
-count=count+1;
-}
-}
-
